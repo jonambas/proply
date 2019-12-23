52375a0ebf091472b6a9be167f9595a95a951e2d
Jon Ambas <jon@jonambas.com>
2019-12-23 18:36:20 -0500

    Add example build

--- example/propspotter.config.js
+++ example/propspotter.config.js
@@ -2,5 +2,6 @@ module.exports = {
   title: "My App",
   include: "/src/**/!(*.spec|*.test).js",
   port: 9000,
-  locationUrl: "https://github.com/jonambas/propspotter/tree/master/example"
+  locationUrl: "https://github.com/jonambas/propspotter/tree/master/example",
+  outputPath: "propspotter-build"
 };
