a39229a591316ccb460a5c8138491e5275ba81e2
Jon Ambas <jon@jonambas.com>
2021-02-07 19:56:08 -0500

    Update example build directory

--- example/proply.config.js
+++ example/proply.config.js
@@ -2,5 +2,5 @@ module.exports = {
   include: "/src/**/!(*.spec|*.test).js",
   port: 9000,
   locationUrl: "https://github.com/jonambas/proply/tree/main/example",
-  outputPath: "../docs"
+  outputPath: "/build"
 };
